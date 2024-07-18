11890e52e514bb7472630f477c676846fec4233e
iris <snazzsinclair@gmail.com>
2024-07-18 01:23:24 -0500

    number, better handling, holds data in a absolute path

--- main.py
+++ main.py
@@ -12,3 +12,15 @@ while True:
             if i+1 != len(command):
                 string += ' '
         find(string)
+    elif command[0] == 'number':
+        if len(command) == 1:
+            numberAll()
+        else:
+            number(command[1])
+    else:
+        string = ''
+        for i in range(len(command)):
+            string += command[i]
+            if i+1 != len(command):
+                string += ' '
+        print('Gamestats: Command not found: ' + string)
--- scripts.py
+++ scripts.py
@@ -1,7 +1,19 @@
 import glob
 import time
+import os
+def getFiles():
+    dir = os.path.expanduser('~')
+    if not os.path.exists(dir + '/.gamestats'):
+        os.mkdir(dir + '/.gamestats')
+    if not os.path.exists(dir + '/.gamestats/games'):
+        os.mkdir(dir + '/.gamestats/games')
+    files = glob.glob(dir + '/.gamestats/games/*')
+    if len(files) == 0:
+        print('please add library game lists to ~/.gamestats/gmames/')
+        exit()
+    return files
 def find(string):
-    files = glob.glob('./games/*')
+    files = getFiles()
     list = []
     for file in files:
         with open(file) as f:
@@ -12,13 +24,36 @@ def find(string):
         games = library[1]
         index = 0
         result = []
-        while index < len(games):
-            for i in range(len(games)):
-                index = i + 1
-                if string.lower() in games[i].lower():
-                    result.append(games[i])
+        for game in games:
+                if string.lower() in game.lower():
+                    result.append(game)
         if result == []:
             continue
-        print(library[0] + ':')
+        print(library[0].split('/')[-1] + ':')
         for i in range(len(result)):
             print(result[i])
+
+def number(string, ret = False):
+    if string == 'all':
+        ret = True
+        string = ''
+    files = getFiles()
+    libs = []
+    for file in files:
+        if string in file:
+            libs.append(file)
+    num = 0
+    for lib in libs:
+        number = 0
+        with open(lib, 'r') as f:
+            games = f.read().splitlines()
+            number = len(games)
+            num += number
+            if not ret:
+                print('There are ' + str(number) + ' games in ' + lib.split('/')[-1])
+    if ret:
+        print("There are " + str(num) + ' games total across all libraries')
+
+
+def numberAll():
+    number('')
